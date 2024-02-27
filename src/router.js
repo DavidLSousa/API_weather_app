a13dbdfa2ec2c1fa486798f710e30d6c8bbc33da
David Sousa <davidlsousa.dev@gmail.com>
2024-02-27 19:48:06 -0300

    edit routes src - vercel.json

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ import express from 'express'
 
 export const router = express.Router()
 
-router.get('/key', (req, res) => {
+router.get('/key', (_, res) => {
 
   const apikey = process.env.APIKEY
 
   return res.json({ apikey: apikey })
 })
+
+router.get('/', (req, res) => res.json({ "teste": 'Teste ok' }))
