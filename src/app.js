e95c3eb016b9a14e98a3f7f2e682cec3f35de157
David Sousa <davidlsousa.dev@gmail.com>
2024-02-28 16:04:38 -0300

    Correcting node verison

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ app.use(router)
 
 app.use((_, res, next) => {
   const baseURLLocal = 'http://127.0.0.1:5503'
-  const baseURL = 'https://weather-app-davidlsousa.netlify.app/'
-  
+  const baseURL = 'https://weather-app-davidlsousa.netlify.app' 
+
   res.setHeader('Access-Control-Allow-Origin', baseURL)
   res.setHeader('Access-Control-Allow-Methods', 'GET')
   next()
